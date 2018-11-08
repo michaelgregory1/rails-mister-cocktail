eca828fa87986e0ce9a3d9cfba4e4b2ea0307063
michael gregory <michaelgregory8976@gmail.com>
2018-11-08 17:13:40 +0000

    database working niceley

---
+++ app/controllers/cocktails_controller.rb
@@ -0,0 +1,43 @@
+class CocktailsController < ApplicationController
+
+  before_action :set_cocktail, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @cocktails = Cocktail.all # GET /restaurants
+  end
+
+  def show          # GET /restaurants/:id
+  end
+
+  def new
+    @cocktail = Cocktail.new
+  end
+
+  def create
+    @cocktail = Cocktail.new(cocktail_params)
+    if @cocktail.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      render :new
+    end
+  end
+
+  def edit          # GET /restaurants/:id/edit
+  end
+
+  def update        # PATCH /restaurants/:id
+  end
+
+  def destroy       # DELETE /restaurants/:id
+  end
+
+  private
+
+  def set_cocktail
+    @cocktail = Cocktail.find(params[:id])
+  end
+
+  def cocktail_params
+    params.require(:cocktail).permit(:name)
+  end
+end
